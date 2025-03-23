b85405d5c8583bf01d0735ba82d2a4a21136a4c0
AnaBiaCosta <anabeatrizdesousacosta@gmail.com>
2025-03-23 20:38:25 -0300

    fix: fix url

--- src/services/RocketApiService.js
+++ src/services/RocketApiService.js
@@ -1,18 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
 class RocketApiService {
     async fetchRocketsPaginated(page, limit) {
-        const res = await fetch(`http://localhost:3000/api/rockets?page=${page}&limit=${limit}`);
+        const res = await fetch(`api/rockets?page=${page}&limit=${limit}`);
         if (!res.ok) throw new Error("Error to find rockets paginated");
         return res.json();
     }
 
     async fetchRockets() {
-        const res = await fetch('https://api.spacexdata.com/v4/rockets');
+        const res = await fetch(`${process.env.SPACEX_DOMAIN_API}/rockets`);
         if (!res.ok) throw new Error("Error to find rockets");
         return res.json();
     }
 
     async fetchRocketDetails(id) {
-        const res = await fetch(`https://api.spacexdata.com/v4/rockets/${id}`);
+        const res = await fetch(`${process.env.SPACEX_DOMAIN_API}/rockets/${id}`);
         if (!res.ok) throw new Error("Error to find rocket details");
         return res.json();
     }
