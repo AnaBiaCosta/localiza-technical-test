94160f4d2107d18638543a534cb1d2524b95881e
AnaBiaCosta <anabeatrizdesousacosta@gmail.com>
2025-03-23 17:53:30 -0300

    feat: add navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { bakbakOne, nunito } from '@/utils/theme';
 import MuiThemeProvider from '@/components/MuiThemeProvider';
 import "./globals.scss";
 
+import { NavBar } from '@/components/NavBar';
+
 export const metadata = {
   title: "Space X",
   description: "Learn about Space X rockets and astronauts",
@@ -11,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${nunito.className} ${bakbakOne.variable} ${nunito.variable}`}>
+        <NavBar />
         <MuiThemeProvider>
           {children}
         </MuiThemeProvider>
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
+const NavBar = () => (
+    <nav className={styles.navbar}>
+        <div className={styles.navbar__content}>
+            <Image
+                src="rocket.svg"
+                width={21}
+                height={52}
+                alt="Rocket"
+            />
+            <p className={styles.navbar__title}>SPACE X</p>
+        </div>
+
+        <ul className={styles['navbar__naviagation-items']}>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/rockets">Rockets</Link></li>
+        </ul>
+    </nav>
+)
+
+export { NavBar }
