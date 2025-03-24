637993327ec5addc6978c63ba968adc898027fd5
AnaBiaCosta <anabeatrizdesousacosta@gmail.com>
2025-03-24 14:33:31 -0300

    feat: add loading responsive design

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,14 +1,28 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './styles.module.scss';
 import { Typography } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-const Loading = () => (
-    <div className={styles.loading}>
-        <Image src="loading.svg" alt="loading" width={700} height={300} />
-         <Typography  variant="body2" color="text.secondary">
-            Loading...
-        </Typography>
-    </div>
-)
+const Loading = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    return (
+        <div className={styles.loading}>
+            <Image
+                src="loading.svg"
+                alt="loading"
+                width={isMobile ? 250 : 700}
+                height={ isMobile ? 100 : 300}
+            />
+            <Typography  variant="body2" color="text.secondary">
+                Loading...
+            </Typography>
+        </div>
+    )
+}
 
 export { Loading }
